95a57bbfc032cae0552441b7c44a0d0c20618282
LeeHebert337 <lee.hebert@sbcglobal.net>
2019-01-03 16:14:27 -0600

    Update helloworld.cpp

--- programExamples/src/helloworld.cpp
+++ programExamples/src/helloworld.cpp
@@ -1,3 +1,21 @@
+/*
+
+Simple program written in C++
+
+to compile on a Linux machine run command    
+
+		 g++ helloworld.cpp -o cppHelloWorld
+
+to execute on a Linux machine run command
+
+		chmod +x cppHelloWorld
+
+		.\cppHelloWorld
+
+
+
+*/
+
 #include <iostream>
 using namespace std;
 
