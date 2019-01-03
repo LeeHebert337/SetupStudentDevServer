fe413ebd4440444b3465d9f83901d7442a83d5e1
LeeHebert337 <lee.hebert@sbcglobal.net>
2019-01-03 16:15:34 -0600

    Update helloworld.c

--- programExamples/src/helloworld.c
+++ programExamples/src/helloworld.c
@@ -3,9 +3,7 @@ Simple program written in C
 to compile on a Linux machine run command    
 		 gcc helloworld.c -o cHelloWorld
 to execute on a Linux machine run command
-		chmod +x cHelloWorld
 		.\cHelloWorld
-
 */
 #include <stdio.h>
 int main(int argc, char *argv[])
