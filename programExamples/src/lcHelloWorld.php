8c670d8606908fb5f1f3af1727c8916b1e3e1f37
LeeHebert337 <lee.hebert@sbcglobal.net>
2019-01-03 17:52:24 -0600

    Simple Linux command line program written in PHP

---
+++ programExamples/src/lcHelloWorld.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php   
+<?php 
+		/*
+			Simple Linux command line program written in PHP.  
+			The php command on the first line this will allow the php program to execute as a Linux command  
+
+
+
+		  to execute on a Linux machine the permissions on the file must include execute 
+		  by using chmod +x lcHelloWorld.php
+		  
+		  You execute the program by running it as a command.  
+		      .\lcHelloWorld.php	
+		      
+		  Note: The extension php on the filename is not required
+		
+		*/
+      echo "Hello, World from PHP as a Linux Command\n";
+?>
