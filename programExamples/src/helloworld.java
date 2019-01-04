f9e0078ec458e2a688a58b2c4d4803f3a7246e4f
LeeHebert337 <lee.hebert@sbcglobal.net>
2019-01-03 19:03:49 -0600

    Simple program written in Java

---
+++ programExamples/src/helloworld.java
@@ -0,0 +1,12 @@
+
+/*
+Simple program written in Java
+to execute on a Linux machine run command
+		java helloworld.java
+
+*/
+class HelloWorld  {
+  public static void main(String args[]) {
+    System.out.println("Hello World from Java.");
+  }
+}
