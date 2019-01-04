899a2bfb33bb06c9de9c8f13ce242b4cb45abc4a
LeeHebert337 <lee.hebert@sbcglobal.net>
2019-01-03 18:57:24 -0600

    Simple Web Page written in PHP

---
+++ programExamples/src/helloworld.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+	Simple Web Page written in PHP.  
+	Any thing outside of the php tags is regular html  notice the comments 
+	in this section are html comments not php comments
+
+
+
+		  The file needs to be in your web server html folder. T
+		  o execute on a Linux machine using a standard apache server the file needs 
+		  to be located in /var/www/html.   You would browse http://localhost/helloworld.php
+-->
+<html>
+ <head>
+  <title>PHP Hello World Web Page</title>
+ </head>
+ <body>
+ 	<?php 
+ 		/*
+ 				This is the actual PHP program to print hello world with some HTML around it.  Notice the comment indicators changes between 
+ 				the top comments and this one
+ 		*/
+ 		echo '<p><h1><center><font color="red">Hello World</font></center></h1></p>'; 
+ 		
+ 	?> 
+ 		
+ </body>
+</html> 
